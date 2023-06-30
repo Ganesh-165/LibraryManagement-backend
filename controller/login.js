1497e032d2e0db4020d3c0975e7b8f05630176a2
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2023-06-30 09:59:32 +0530

    Update login.js

--- controller/login.js
+++ controller/login.js
@@ -5,7 +5,6 @@ const validateData= async(req,res)=>{
     try{
         const {email,password,type}=req.body
         const userdata=await collection.findOne({email:email}).exec();
-        res.setHeader({'Access-Control-Allow-Origin'},{true});
         if(!userdata){
             return res.status(200).json({success:false, message:'use Emailid:admin@gmail.com Password:12345'})
         }
