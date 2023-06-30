a28bec9edb042e26dcc2fcf0d36a98f6e4808700
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2023-06-30 08:53:32 +0530

    Update Connection.js

--- database/Connection.js
+++ database/Connection.js
@@ -1,6 +1,6 @@
 const mongoose =  require('mongoose');
 
 const connection = async ()=>{
-   await mongoose.connect('mongodb+srv://ganesh:ganesh2003@cluster0.nixttey.mongodb.net/?retryWrites=true&w=majority').catch((error)=>console.log(error));
+   await mongoose.connect('mongodb+srv://ganesh:ganesh2003@cluster0.nixttey.mongodb.net/LibraryManagament?retryWrites=true&w=majority').catch((error)=>console.log(error));
 }
 module.exports=connection;
