e68138524b1829cf237e05e0d07e1f65b5fdf713
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2023-06-30 08:13:52 +0530

    Update dashboard.js

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -3,7 +3,6 @@ const collection = require('../database/Schema')
 const getData = async(req,res)=>{
     const {email} = req.session;
     const name = await collection.findOne({email:email}).exec();
-    res.setHeader("Access-Control-Allow-Credentials","true");
     res.json({name:name});
 }
 module.exports={getData}
