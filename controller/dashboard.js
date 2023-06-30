60ce78fc2e06f31415bf66f933650b9b900b136e
Ganesh-165 <105793754+Ganesh-165@users.noreply.github.com>
2023-06-30 07:46:54 +0530

    Update dashboard.js

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -1,10 +1,8 @@
 const collection = require('../database/Schema')
 
 const getData = async(req,res)=>{
-    const {email} = req.session
-    console.log(email)
+    const {email} = req.body;
     const name = await collection.findOne({email:email}).exec();
-    console.log(name)
     res.json({name:name});
 }
 module.exports={getData}
